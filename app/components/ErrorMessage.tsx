90ad82b9592c16fc3914b9d04bed35e447372af0
Krishna Rajagopal <krishan.rajagopal@gmail.com>
2024-01-14 20:38:51 -0800

    Extract error message component for reusability

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import { Callout, CalloutText } from '@radix-ui/themes'
+import React, { PropsWithChildren } from 'react'
+
+const ErrorMessage = ({children}: PropsWithChildren) => {
+        if(!children) return null
+  return (
+        <Callout.Root color="red"><Callout.Text>{children}</Callout.Text></Callout.Root>
+  )
+}
+
+export default ErrorMessage
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useRouter } from "next/navigation";
 import{zodResolver} from "@hookform/resolvers/zod"
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -35,7 +36,7 @@ const NewIssuePage = () => {
       <TextField.Root>
         <TextField.Input placeholder='Title' {...register("title")} />
       </TextField.Root>
-      {errors.title && <Callout.Root color="red"><CalloutText>{errors.title.message}</CalloutText></Callout.Root>}
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
       <Controller
         name='description'
         control={control}
@@ -43,7 +44,8 @@ const NewIssuePage = () => {
           <SimpleMDE placeholder='Description' {...field} />
         )}
       />
-      {errors.description && <Callout.Root color="red"><CalloutText>{errors.description.message}</CalloutText></Callout.Root>}
+      <ErrorMessage>{errors.description?.message}</ErrorMessage>
+   
 
       <Button>Submit New Issue</Button>
     </form>
